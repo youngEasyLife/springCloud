dddcacd924c41d71f588c987fac8834c653f7a0e
wpy <365597937@qq.com>
2019-01-23 22:19:53 +0800

    spring cloud 初建立

---
+++ hello-spring-cloud-eureka/src/main/java/com/young/hello/spring/cloud/eureka/EurekaApplication.java
@@ -0,0 +1,20 @@
+package com.young.hello.spring.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: yang
+ * Date: 2019/1/23
+ * Time: 21:57
+ * Desc:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
