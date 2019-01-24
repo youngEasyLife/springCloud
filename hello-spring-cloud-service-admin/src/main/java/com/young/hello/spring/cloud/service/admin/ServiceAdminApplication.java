8f726935fad21a5f8b0c371f5bfbe44c7715fea5
peiyoung <wangpeiyang@yansu.com>
2019-01-24 15:44:02 +0800

    服务注册与发现

---
+++ hello-spring-cloud-service-admin/src/main/java/com/young/hello/spring/cloud/service/admin/ServiceAdminApplication.java
@@ -0,0 +1,19 @@
+package com.young.hello.spring.cloud.service.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * ClassName : ServiceAdminApplication
+ * Description :启动类 开启客户端
+ * Author : young
+ * Date: 2019/1/24 15:07
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ServiceAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAdminApplication.class, args);
+    }
+}
---
+++ hello-spring-cloud-service-admin/src/main/java/com/young/hello/spring/cloud/service/admin/controller/AdminController.java
@@ -0,0 +1,26 @@
+package com.young.hello.spring.cloud.service.admin.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ClassName : AdminController
+ * Description :服务的注册与发现
+ * Author : young
+ * Date: 2019/1/24 15:23
+ **/
+@RestController
+public class AdminController {
+
+    @Value("${server.port}")
+    private String port;
+
+
+    @GetMapping("/hi")
+    public String Hi(String message) {
+        return String.format("your message is %s ,port is %s", message, port);
+    }
+}
+
+
