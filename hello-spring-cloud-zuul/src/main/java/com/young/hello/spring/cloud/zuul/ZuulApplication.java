60f104188cda1e036da890725696242b59954107
peiyoung <365597937@qq.com>
2019-01-28 17:07:23 +0800

    使用路由网关统一访问接口Zuul

---
+++ hello-spring-cloud-zuul/src/main/java/com/young/hello/spring/cloud/zuul/ZuulApplication.java
@@ -0,0 +1,24 @@
+package com.young.hello.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * ClassName : ZuulApplication
+ * Description :Zuul 简介
+ * Zuul 的主要功能是路由转发和过滤器。路由功能是微服务的一部分，
+ * 比如 /api/user 转发到到 User 服务，/api/shop 转发到到 Shop 服务。
+ * Zuul 默认和 Ribbon 结合实现了负载均衡的功能
+ * Author : young
+ * Date: 2019/1/28 16:48
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
