060f16db100c272d173ff8f68daf6c01f7a1b832
peiyoung <wangpeiyang@yansu.com>
2019-01-28 11:39:24 +0800

    Hystrix引用于ribbon模式

--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/young/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/young/hello/spring/cloud/web/admin/ribbon/WebAdminRibbonApplication.java
@@ -3,15 +3,18 @@ package com.young.hello.spring.cloud.web.admin.ribbon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 /**
  * ClassName : WebAdminRibbonApplication
  * Description :通过 @EnableDiscoveryClient 注解注册到服务中心
+ * EnableHystrix支持熔断
  * Author : young
  * Date: 2019/1/24 17:12
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableHystrix
 public class WebAdminRibbonApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminRibbonApplication.class, args);
--- hello-spring-cloud-web-admin-ribbon/src/main/java/com/young/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
+++ hello-spring-cloud-web-admin-ribbon/src/main/java/com/young/hello/spring/cloud/web/admin/ribbon/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.young.hello.spring.cloud.web.admin.ribbon.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -7,8 +8,8 @@ import org.springframework.web.client.RestTemplate;
 /**
  * ClassName : AdminService
  * Description :在这里我们直接用的程序名替代了具体的 URL 地址，
- *              在 Ribbon 中它会根据服务名来选择具体的服务实例，
- *              根据服务实例在请求的时候会用具体的 URL 替换掉服务名，代码如下：
+ * 在 Ribbon 中它会根据服务名来选择具体的服务实例，
+ * 根据服务实例在请求的时候会用具体的 URL 替换掉服务名，代码如下：
  * Author : young
  * Date: 2019/1/24 17:24
  **/
@@ -18,7 +19,12 @@ public class AdminService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @HystrixCommand(fallbackMethod = "hiError")
     public String sayHi(String message) {
         return restTemplate.getForObject("http://HELLO-SPRING-CLOUD-SERVICE-ADMIN/hi?message=" + message, String.class);
+    }
+
+    public String hiError(String message) {
+        return String.format("your message is %s ,but request error", message);
     }
 }
