2fbfac24c5a86cf2dcc85fc87465342b86bfbf44
peiyoung <wangpeiyang@yansu.com>
2019-01-28 10:58:16 +0800

    Feign

--- hello-spring-cloud-web-admin-feign/src/main/java/com/young/hello/spring/cloud/web/admin/feign/service/AdminService.java
+++ hello-spring-cloud-web-admin-feign/src/main/java/com/young/hello/spring/cloud/web/admin/feign/service/AdminService.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
  **/
 @FeignClient(value = "hello-spring-cloud-service-admin")
 public interface AdminService {
-
     @RequestMapping(value = "hi", method = RequestMethod.GET)
     public String sayHi(@RequestParam(value = "message") String message);
 }
